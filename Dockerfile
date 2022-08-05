f37a199f09e067e4d73627a4e5d87fcb5acac5c8
funstea <adam.funsten@autodesk.com>
2022-08-05 10:53:37 -0700

    a

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+# docker build -t alc .
+
+# refer to https://docs.amazonaws.cn/en_us/lambda/latest/dg/images-create.html
+#FROM scratch 
+FROM public.ecr.aws/lambda/nodejs:latest
+#FROM node:16-alpine
+
+# Assumes your function is named "app.js", and there is a package.json file in the app directory 
+COPY lambda-entrypoint.sh /lambda-entrypoint.sh
+#ADD aws-lambda-rie-x86_64 /usr/local/bin/aws-lambda-rie
+COPY /var/runtime/bootstrap /var/runtime/bootstrap
+RUN chmod +x /lambda-entrypoint.sh /var/runtime/bootstrap
+COPY var/task/app.js package.json  ${LAMBDA_TASK_ROOT}
+
+# Install NPM dependencies for function
+RUN npm install
+
+WORKDIR /var/task
+
+ENTRYPOINT [ "/bin/sh" ]
+CMD [ "/lambda-entrypoint.sh", "app.handler" ]
---
+++ var/task/app.js
@@ -0,0 +1,34 @@
+#!/usr/local/bin/node
+// jshint esversion: 8
+// npm test
+
+const fetch = require('node-fetch');
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3();
+
+// BUCKET="$BUCKET" node -p -e 'const app=require("./var/task/app.js"); app.handler({"key": "ok"})'
+module.exports.handler = (event, context, callback) => {
+  console.log("app.handler( event:" + JSON.stringify(event, null, 2) + ")" );
+  let env = "ENVIRONMENT VARIABLES\n" + JSON.stringify(process.env, null, 2);
+  //console.log(env);
+  //console.warn("warn example.");
+  //return env;
+  
+  // fetch(event.url)
+  //   .then((response) => {
+  //     if (response.ok) {
+  //       return response;
+  //     }
+  //     return Promise.reject(new Error(
+  //           `Failed to fetch ${response.url}: ${response.status} ${response.statusText}`));
+  //   })
+  //   .then(response => response.buffer())
+  //   .then(buffer => (
+      s3.putObject({
+        Bucket: process.env.BUCKET,
+        Key: event.key,
+        Body: "<h1>works</h1>",
+      }).promise();
+  //   ))
+  //   .then(v => callback(null, v), callback);
+};

b2741f80d0641fded359a0425e9ae6062d7b8644
afunsten <afunsten@me.com>
2022-08-08 22:42:31 -0700

    Update app.js

--- var/task/app.js
+++ var/task/app.js
@@ -12,19 +12,28 @@ module.exports.myHandler = (event, context, callback) => {
   //console.log("app.handler( event:" + JSON.stringify(event, null, 2) + ")" );
   let data = JSON.stringify(process.env, null, 2);
   //console.log(data);
-  event.s3FilePath = "/tmp/" + event.key + ".txt";
-  require("./base.js").writeJSONFile(event.s3FilePath, data);
-  //require("./base.js").writeFile(event.s3FilePath, data);
+  process.env.Output_FilePath = "/tmp/env." + event.key + ".txt";
+  require("./base.js").writeJSONFileV1(process.env.Output_FilePath, data);
+  // require("./base.js").writeFileV1( process.env.Output_FilePath, data, 'w');
 
+  // get s3 file
+  let getObject_response = require("./s3.js").getObjectV1(process.env.S3_BUCKET, process.env.S3_Input_FilePath).then((res) => {
+    console.log(JSON.stringify(res, null, 2));
+    console.log(res.Body);
+    if (res.status == "ok") { console.log(value.data.Body.toString('utf-8')); }
+  });
+
+  // put s3 file
   try {
     return new Promise((resolve, reject) => {
-       response = require("./s3.js").putObject(process, event, data, response);
-    }).then((response) => {
-      if ( response.status == "ok") { context.succeed(JSON.stringify(response)); }
-      if ( response.status == "fail") { context.fail(JSON.stringify(response)); }
+      let putObject_response = require("./s3.js").putObjectV1(process.env.S3_BUCKET, process.env.S3_Output_FilePath, data, response);
+    }).then((putObject_response) => {
+      if ( putObject_response.status == "ok") { context.succeed(JSON.stringify(response)); }
+      if ( putObject_response.status == "fail") { context.fail(JSON.stringify(response)); }
     });
-  } catch (response) {
-    context.fail(JSON.stringify(response));
+  } catch (putObject_response) {
+    context.fail(JSON.stringify(putObject_response));
   }
-
+  
+  return response;
 };

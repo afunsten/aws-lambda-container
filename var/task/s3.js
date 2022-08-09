4842288220bbbe015486987d2054e793f208c5bd
afunsten <afunsten@me.com>
2022-08-08 22:42:14 -0700

    Update s3.js

--- var/task/s3.js
+++ var/task/s3.js
@@ -3,24 +3,44 @@
 const AWS = require('aws-sdk');
 const s3 = new AWS.S3();
 
-module.exports.putObject = function(process, event, data, response={}) {
-    //  recursive create userDirectory
-    //  BUCKET="$BUCKET" node -e 'let event={}; let response={}; let res=require("./s3.js").putObject(event, process, response); console.log(res)'
-    let s3FilePath = event.s3FilePath.replace(/^\/+/g, '');
+module.exports.getObjectV1 = function(bucket, objectKey) {
+// S3_BUCKET="$BUCKET" node -e 'let res=require("./s3.js").getObjectV1(process.env.S3_BUCKET, "app/input.txt", {}); console.log(res)'
+// Retrieve the object
+    let response = {};
+    let s3FilePath = objectKey.replace(/^\/+/g, '');
+    console.log(`getObjectV1: ${bucket}/${s3FilePath}`);
+    const params = {
+        Bucket: bucket,
+        Key: objectKey
+    };
+    response = s3.getObject(params).promise().then(res => {
+        console.log(`getObjectV1 : ${bucket}/${s3FilePath} : ok`);
+        //console.log(res.Body.toString('utf-8'));
+        require("./base.js").writeFileV1(`/tmp/${s3FilePath}`, res.Body.toString('utf-8'), 'w' );
+        return res;
+    }).catch(err => {
+        console.log(`getObjectV1: ${bucket}/${s3FilePath} : failed`, err);
+        response.status = "fail";
+        return err;
+    });
+    return response;
+};
+
+module.exports.putObjectV1 = function(bucket, objectKey, data, response={}) {
+//  recursive create userDirectory
+//  S3_BUCKET="$BUCKET" node -e 'let res=require("./s3.js").putObjectV1(process.env.S3_BUCKET, s3FilePath, response); console.log(res)'
+    let s3FilePath = objectKey.replace(/^\/+/g, '');
     s3.putObject({
-        Bucket: process.env.BUCKET,
+        Bucket: bucket,
         Body: data,
         Key: s3FilePath
-       })
-        .promise()
-        .then(res => {
-         console.log(`putObject: ${process.env.BUCKET}/${s3FilePath} : ok`);
+       }).promise().then(res => {
+         console.log(`putObject: ${bucket}/${s3FilePath} : ok`);
          response.status = "ok";
          response.res=res;
          return response;
-        })
-        .catch(err => {
-         console.log(`putObject : ${process.env.BUCKET}/${s3FilePath} : failed`, err);
+        }).catch(err => {
+         console.log(`putObject : ${bucket}/${s3FilePath} : failed`, err);
          response.status = "fail";
          response.err=err;
          return response;

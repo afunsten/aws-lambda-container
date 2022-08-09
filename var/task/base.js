1261588ee1618aa234fb540afe6e05fce95c98db
afunsten <afunsten@me.com>
2022-08-08 22:41:53 -0700

    Update base.js

--- var/task/base.js
+++ var/task/base.js
@@ -6,6 +6,7 @@
 
 const fs = require("fs");
 const path = require('path');
+
 const callback = err => {
     if (err) {throw err;}
 };
@@ -21,43 +22,50 @@ module.exports.createDirectory = function(userDirectory) {
         } else {
             console.log("createDirectory: " + userDirectory);
             return fs.mkdirSync(userDirectory, { recursive: true });
-            //fs.mkdir(userDirectory, callback)
         }
 };
 
-module.exports.writeJSONFile = function(filePath, userData, sort=true) {
+module.exports.writeJSONFileV1 = function(filePath, userData, sort=false) {
     // stringify a json object into a pretty file
-    //  node -e 'require("./base.js").writeJSONFile("./tests/testFile.json", {});'
-    //console.log(userData);
-        if (sort) {
-            const sorted_userData = {};
-            Object.keys(userData).sort().forEach(function(key) {
-                sorted_userData[key] = userData[key];
+    //  node -e 'require("./base.js").writeJSONFileV1("./tests/testFile.json", {});'
+    if (sort) {
+        const sorted_userData = {};
+        Object.keys(userData).sort().forEach(function(key) {
+            sorted_userData[key] = userData[key];
+        });
+        userData = sorted_userData;
+    }
+    const userDirectory = path.dirname(filePath);
+    try {
+        exports.createDirectory(userDirectory);
+    } catch (error) {console.log( error );}
+    try {
+        console.log("writeJSONFileV1: " + filePath);
+        let data = JSON.stringify(userData, null, 2);
+        if ( typeof userData !== 'undefined' ) { 
+            fs.writeFile(filePath, data, (err) => {
+                if (err) throw err;
+                console.log('writeJSONFileV1: Data written to file');
             });
-            userData = sorted_userData;
-        }
-        const userDirectory = path.dirname(filePath);
-        try {
-            exports.createDirectory(userDirectory);
-        } catch (error) {console.log( error );}
-        try {
-            console.log("writeJSONFile: " + filePath);
-            if ( typeof userData !== 'undefined' ) { fs.writeFile(filePath, JSON.stringify(userData, null, 3), callback); }
-            else { console.log("WARN: userData is undefined");
-                fs.writeFile(filePath, JSON.stringify("undefined", null, 3), callback);
-            }      
-        } catch (error) {console.log( error + " ERROR: can not writeFile ");}
+         }
+        else { console.log("WARN: writeJSONFileV1: userData is undefined");
+            fs.writeFile(filePath, data, (err) => {
+                if (err) throw err;
+                console.log('writeJSONFileV1: Data written to file');
+            });
+        }      
+    } catch (error) {console.log( error + " ERROR: can not writeFile ");}
 };
 
-module.exports.writeFile = function(filePath="app.log", data="message") {
-//  node -p 'require("./base.js").writeFile(filePath="app.log", message="message");'
+module.exports.writeFileV1 = function(filePath="app.log", data="message", flags='a') {
+//  node -p 'require("./base.js").writeFileV1(filePath="app.log", data="message", 'w' );'
     if (fs.existsSync(filePath)) {
-        console.log(`writeFile : ` + filePath );
+        console.log(`writeFileV1: ` + filePath );
     } else {
-        console.log(`writeFile : ` + filePath );
+        console.log(`writeFileV1: ` + filePath );
         exports.createDirectory(path.dirname(filePath));
     }
-    var stream = fs.createWriteStream(filePath, { flags: 'a' });
-    stream.write(`${data}\n`);
+    var stream = fs.createWriteStream(filePath, { flags: flags });
+    stream.write(data);
     stream.on('drain', function () { });
 };
